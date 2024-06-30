b5fa7f6677449e5b99589578b1434bf284abbe73
RidloGhifary <ridloghfry@gmail.com>
2024-06-30 20:02:28 +0700

    fix: navbar styling bug

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     {
       href: "/",
       label: "Dashboard",
-      active: pathname.startsWith("/"),
+      active: pathname === "/",
     },
     {
       href: "/issues",
