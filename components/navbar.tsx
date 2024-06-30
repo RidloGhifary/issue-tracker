c9d73ba91929002d41ad6924997dd4845283c202
RidloGhifary <ridloghfry@gmail.com>
2024-06-30 19:37:52 +0700

    feat: create a navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+export default function Navbar() {
+  const pathname = usePathname();
+
+  const links = [
+    {
+      href: "/",
+      label: "Dashboard",
+      active: pathname.startsWith("/"),
+    },
+    {
+      href: "/issues",
+      label: "Issues",
+      active: pathname.startsWith("/issues"),
+    },
+  ];
+
+  return (
+    <div className="bg-white p-4 border-b">
+      <nav className="max-w-7xl container mx-auto space-x-5">
+        {links.map((link) => (
+          <Link
+            href={link.href}
+            key={link.label}
+            className={classNames({
+              "text-gray-600": true,
+              "text-black font-semibold": link.active,
+            })}>
+            {link.label}
+          </Link>
+        ))}
+      </nav>
+    </div>
+  );
+}
