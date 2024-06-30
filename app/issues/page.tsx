bbbec0b80de02b0842acfe0fea2de5bb70eae84c
RidloGhifary <ridloghfry@gmail.com>
2024-06-30 21:27:28 +0700

    feat: auto adding using useEffect

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,24 @@
-import { getAllIssues } from "@/actions/issues";
+"use client";
 
-export default async function Issues() {
-  const issues = await getAllIssues();
+import { getAllIssues } from "@/actions/issues";
+import { useEffect, useState } from "react";
+
+export default function Issues() {
+  const [issues, setIssues] = useState<any>();
+
+  const fetchData = async () => {
+    const issues = await getAllIssues();
+    setIssues(issues);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="max-w-7xl container mx-auto">
       <ol>
-        {issues?.map((issue) => (
+        {issues?.map((issue: any) => (
           <li key={issue.id}>- {issue.title}</li>
         ))}
       </ol>
